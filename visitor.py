e501635ce05a3e7e46536f136150cc5bbba283df
Gyanendra Mishra <anomaly.the@gmail.com>
2017-01-11 23:10:57 +0800

    almost all ciphers and crypto checks are working, commiting for backup

--- visitor.py
+++ visitor.py
@@ -4,7 +4,7 @@ class RecursiveVisitor(ast.NodeVisitor):
 
     report = []
     filename = ''
-    usual_suspects = set(['password', 'pass', 'secret', 'token', 'key', 'passwd', 'pwd'])
+    usual_suspects = set(['password', 'pass', 'secret', 'token', 'passwd', 'pwd'])
     fun_module = {}
 
     bad_imports = {
@@ -31,20 +31,23 @@ class RecursiveVisitor(ast.NodeVisitor):
         'pickle': {'functions' : ['loads', 'load', 'Unpickler'], 'severity': 'medium', 'text':'possible security issue with pickle'},
         'cPickle': {'functions' : ['loads', 'load', 'Unpickler'], 'severity': 'medium', 'text':'possible security issue with pickle'},
         'hashlib': {'functions' : ['md5'], 'severity': 'medium', 'text':'md-5 can be cracked, use something stronger'},
-        'Crypto.Hash': {'functions':['MD2', 'MD4', 'MD5'], 'severity': 'medium','text':'md-5 can be cracked, use something stronger'},
+        'Crypto.Hash': {'functions':['MD2.new', 'MD4.new', 'MD5.new'], 'severity': 'medium','text':'md-5 can be cracked, use something stronger'},
         'cryptography.hazmat.primitives.hashes': {'functions': ['MD5'], 'severity': 'medium', 'text':'md-5 can be cracked use something stronger'},
         'marshal': {'functions': ['loads', 'load'], 'severity': 'medium', 'text': 'deserialization with marshall is dangerous'},
-        'Crypto.Cipher': {'functions': ['ARC2','ARC4', 'Blowfish', 'DES', 'XOR'], 'severity': 'High', 'text': 'use AES insted'},
-        'cryptography.hazmat.primitives.ciphers.algorithms': {'functions': ['ARC4', 'BlowFish', 'IDEA']: 'severity': 'HIGH', 'text': 'use AES insted'},
-        'cryptography.hazmat.primitives.ciphers.modes': {'functions': ['ECB'], 'severity': 'Medium', 'text': 'ECB has patterns use CBC instead?'}
+        'Crypto.Cipher': {'functions': ['ARC2.new','ARC4.new', 'Blowfish.new', 'DES.new', 'XOR.new'], 'severity': 'High', 'text': 'use AES insted'},
+        'cryptography.hazmat.primitives.ciphers': {'functions': ['algorithms.ARC4', 'algorithms.BlowFish', 'algorithms.IDEA'], 'severity': 'HIGH', 'text': 'use AES insted'},
+        'cryptography.hazmat.primitives.ciphers.modes': {'functions': ['ECB'], 'severity': 'Medium', 'text': 'ECB has patterns use CBC instead?'},
         'tempfile': {'functions': ['mktemp_q'], 'severity': 'medium', 'text': 'the function is insecure and deprecated'},
-        'standard1': {'functions': ['eval'], 'severity': 'medium', 'text': 'use ast.literal_eval instead'},
         'django.utils.safestring.': {'functions': ['mark_safe'], 'severity': 'medium', 'text': 'possible xss, avoid using?'},
         'httplib': {'functions': ['HTTPSConnection'], 'severity': 'medium', 'text': 'use of HTTPSConnection doesnt provide safety'},
         'http.client': {'functions': ['HTTPSConnection'], 'severity': 'medium', 'text': 'use of HTTPSConnection doesnt provide safety'},
         'six.moves.http_client': {'functions': ['HTTPSConnection'], 'severity': 'medium', 'text': 'use of HTTPSConnection doesnt provide safety'},
-        'random': {'functions': ['random', 'randrange', 'choice', 'uniform', 'triangular'], 'severity': 'low', 'not suitable for crypto purposes'},
+        'random': {'functions': ['random', 'randrange', 'choice', 'uniform', 'triangular'], 'severity': 'low', 'text': 'not suitable for crypto purposes'}
     }
+
+    bad_functions = [x['functions'] for x in bad_calls.values()]
+    bad_functions = set([j for i in bad_functions for j in i])
+
 
 
     def recursive(func):
@@ -115,6 +118,11 @@ class RecursiveVisitor(ast.NodeVisitor):
     def visit_Import(self, node):
         lineno = node.lineno
         for alias in ast.iter_child_nodes(node):
+            if alias.asname:
+                self.fun_module[alias.asname] = {'module': alias.name, 'name': alias.name}
+            else:
+                self.fun_module[alias.name] = {'module': alias.name, 'name': alias.name}
+
             if isinstance(alias, ast.alias) and alias.name in self.bad_imports:
                 self.add_to_report('bad-import', node.lineno, self.bad_imports[alias.name]['severity'], 'high', self.bad_imports[alias.name]['text'])
 
@@ -140,6 +148,12 @@ class RecursiveVisitor(ast.NodeVisitor):
     def visit_Call(self, node):
 
         if isinstance(node.func, ast.Name):
+
+            # bad_calls direct
+            if self.fun_module.get(node.func.id, {}).get('module', None) in self.bad_calls and self.fun_module[node.func.id]['name'] in self.bad_functions:
+                module = self.fun_module.get(node.func.id, {}).get('module', None)
+                self.add_to_report('bad-calls', node.lineno, self.bad_calls[module]['severity'], 'high', self.bad_calls[module]['text'])
+
             #bad imports
             if node.func.id == '__import__' and len(node.args) > 0:
                 module_name = node.args[0]
@@ -147,7 +161,7 @@ class RecursiveVisitor(ast.NodeVisitor):
                     self.add_to_report('bad-import', node.lineno, self.bad_imports[module_name.s]['severity'], 'high', self.bad_imports[module_name.s]['text'])
 
             #jinja-2-xss
-            if node.func.id == 'Environment' and (self.fun_module.get(node.func.id, {}).get('module', None) == 'jinja2' and self.fun_module[node.func.id]['name'] == 'Environment'):
+            if (self.fun_module.get(node.func.id, {}).get('module', None) == 'jinja2' and self.fun_module[node.func.id]['name'] == 'Environment'):
                 auto_escape_found=False
                 for keyword in node.keywords:
                     if keyword.arg=='autoescape' and (getattr(keyword.value, 'id', None)=='False' or getattr(keyword.value, 'value', None) == False):
@@ -164,6 +178,23 @@ class RecursiveVisitor(ast.NodeVisitor):
 
         if isinstance(node.func, ast.Attribute):
 
+            func = node.func
+            call = []
+
+            while isinstance(func.value, ast.Attribute):
+                call.append(func.attr)
+                func = func.value
+            call = call.append(func.attr)
+
+            module = None
+
+
+            # bad-calls
+            # handles as imports properly
+            if self.fun_module.get(getattr(node.func.value, 'id', None), {}).get('module', None) in self.bad_calls and (node.func.attr in self.bad_functions or (str(self.fun_module[node.func.value.id]['name']) + '.' +  str(node.func.attr)) in self.bad_functions):
+                module = self.fun_module.get(getattr(node.func.value, 'id', None), {}).get('module', None)
+                self.add_to_report('bad-call', node.lineno, self.bad_calls[module]['severity'], 'high', self.bad_calls[module]['text'])
+
             # flask debug
             if getattr(node.func.value, 'id', None) == 'app' and node.func.attr == 'run':
                 for keyword in node.keywords:
@@ -171,7 +202,8 @@ class RecursiveVisitor(ast.NodeVisitor):
                         self.add_to_report('debug-enabled', node.lineno, 'medium', 'high', 'running an application with debug enabled can be dangerous')
 
             # jinja-2-xss
-            if getattr(node.func.value, 'id', None) == 'jinja2' and node.func.attr == 'Environment':
+            # new version takes care of as imports
+            if self.fun_module.get(getattr(node.func.value, 'id', None), {}).get('module', None) == 'jinja2' and node.func.attr == 'Environment':
                 auto_escape_found=False
                 for keyword in node.keywords:
                     if keyword.arg=='autoescape' and (getattr(keyword.value, 'id', None)=='False' or getattr(keyword.value, 'value', None) == False):
